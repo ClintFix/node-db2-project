f660d2ed46b9536dddb01cc0f19a41bfbd2edae1
ClintFix <clint.fix@gmail.com>
2021-04-13 12:57:56 -0600

    exported MW and added error handling middleware to router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -4,4 +4,10 @@ const Cars = require('./cars-model');
 
 //Routers
 
+
+//Error Handling
+router.use((err, _, res,) => {
+    res.status(500).json({message: err.message, stack: err.stack });
+});
+
 module.exports = router;
